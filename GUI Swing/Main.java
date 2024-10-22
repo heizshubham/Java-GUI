c82cddacc04b14da8dd73d1923b172dd5e21696b
heizshubham <shubhamp01947@gmail.com>
2024-10-23 02:25:09 +0530

    gui swing

---
+++ GUI Swing/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // JFrame = a GUI window to add components to
+
+        JFrame frame = new JFrame(); //creates a frame
+
+    }
+}
